27e47f149d7ca1b4360bc7d41fb985b91d3220ac
andrew-marquezin <andrewmmarinho@gmail.com>
2024-05-16 16:20:39 -0300

    requisito 3 completo

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    processed_file = False
+    for file in instance.queue:
+        if file["nome_do_arquivo"] == path_file:
+            processed_file = True
+            break
+
+    lines = txt_importer(path_file)
+    item = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+
+    if not processed_file:
+        instance.enqueue(item)
+    print(item)
 
 
 def remove(instance):
