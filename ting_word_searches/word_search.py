a8b628a7da60249fab5f9e12d859e9566e8e5dbc
andrew-marquezin <andrewmmarinho@gmail.com>
2024-05-16 19:32:47 -0300

    requisito 7 foi

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    existing = []
+
+    for file in instance.queue:
+        occurencies = [
+            {"linha": index + 1}
+            for index, line in enumerate(file["linhas_do_arquivo"])
+            if word.lower() in line.lower()
+        ]
+
+        if len(occurencies) == 0:
+            return []
+
+        existing.append({
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": occurencies
+        })
+
+    return existing
 
 
 def search_by_word(word, instance):
