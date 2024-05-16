a7c23791e52aaf69a5bc616b353340fbe0deccb6
andrew-marquezin <andrewmmarinho@gmail.com>
2024-05-16 15:14:10 -0300

    requisito 2 completo

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file, "r") as file:
+            return [line.strip('\n') for line in file]
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
