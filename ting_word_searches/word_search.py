514844a23e26922db9d6f0f7ecb94d7ccb402f84
andrew-marquezin <andrewmmarinho@gmail.com>
2024-05-16 21:02:55 -0300

    finalizando projeto

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,8 +1,10 @@
-def exists_word(word, instance):
+def exists_word(word, instance, show_line=False):
     existing = []
 
     for file in instance.queue:
         occurencies = [
+            {"linha": index + 1, "conteudo": line}
+            if show_line else
             {"linha": index + 1}
             for index, line in enumerate(file["linhas_do_arquivo"])
             if word.lower() in line.lower()
@@ -21,4 +23,4 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return exists_word(word, instance, show_line=True)
